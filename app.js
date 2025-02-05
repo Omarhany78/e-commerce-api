5fb10abb0d9fce398d1fd0c75c6f1fbe323da9e6
Omar Aref <omarhanyaref55@gmail.com>
2025-02-05 22:56:15 +0200

    m

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
